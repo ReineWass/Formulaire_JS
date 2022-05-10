f610f89cbbabdfe8fe2c644b917cda9c6fd9fecb
iMac G1 <imacg1@iMac-de-Energy-26.local>
2022-05-10 09:20:45 +0000

    correction

--- index.js
+++ index.js
@@ -3,24 +3,26 @@
  //récupération des données
  let noms = document.getElementById("nom");
  let ppnom = document.getElementById("pnom");
+//  afficher la valeur de la variable noms
  ppnom.innerHTML = noms.value;
 
 let email = document.getElementById("emails");
 let waemail = document.getElementById("w_email");
+// afficher la valeur de la variable email 
 waemail.innerHTML = email.value;
 
 let satis = document.getElementById("sat");
 let wasat = document.getElementById("w_sat");
+// afficher la valeur de la variable satis
 wasat.innerHTML = satis.value;
 
 let coachs = document.getElementById("coach");
 let wacoach = document.getElementById("w_coach");
+// afficher la valeur de la variable coach
 wacoach.innerHTML = coachs.value;
 
 let experiences = document.getElementById("experience");
 let waexperience = document.getElementById("w_experience");
+// afficher la valeur de la variable experiences
 waexperience.innerHTML = experiences.value;
 } 
-
-
-
